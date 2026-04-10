e0d2f60ec382d5c313ac2574e0f00ca3bcb2dfe6
Agasthi16 <arunaagasthi@gmail.com>
2026-04-10 16:16:03 +0530

    init ZoneRepository

--- e-commerce/zoneService/src/main/java/lk/ijse/zoneService/repository/ZoneRepository.java
+++ e-commerce/zoneService/src/main/java/lk/ijse/zoneService/repository/ZoneRepository.java
@@ -1,4 +1,7 @@
 package lk.ijse.zoneService.repository;
 
-public class ZoneRepository {
+import lk.ijse.zoneService.entity.ZoneEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<ZoneEntity,String> {
 }
