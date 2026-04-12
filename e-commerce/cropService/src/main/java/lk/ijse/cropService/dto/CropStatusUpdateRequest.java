390d185e469c21f3dd3aa4d9554335cd6efadef3
Agasthi16 <arunaagasthi@gmail.com>
2026-04-12 14:01:04 +0530

    updated Apigateway

--- e-commerce/cropService/src/main/java/lk/ijse/cropService/dto/CropStatusUpdateRequest.java
+++ e-commerce/cropService/src/main/java/lk/ijse/cropService/dto/CropStatusUpdateRequest.java
@@ -1,4 +1,13 @@
 package lk.ijse.cropService.dto;
 
 public class CropStatusUpdateRequest {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- e-commerce/cropService/src/main/java/lk/ijse/cropService/repository/CropRepository.java
+++ e-commerce/cropService/src/main/java/lk/ijse/cropService/repository/CropRepository.java
@@ -1,4 +1,10 @@
 package lk.ijse.cropService.repository;
 
-public class CropRepository {
+import lk.ijse.cropService.entity.Crop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CropRepository extends JpaRepository<Crop, String> {
+
 }
